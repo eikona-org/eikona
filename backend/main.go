e882c6377faf1c7cdcb41de4cc9cbfb9639b81ec
Dominik Castelberg <dominik.castelberg@ost.ch>
2021-03-17 14:07:46 +0100

    Restructure ORM usage

--- backend/main.go
+++ backend/main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"github.com/imgProcessing/backend/v2/data"
 	"github.com/imgProcessing/backend/v2/web"
 )
 
 func main() {
-	context := data.GetContext()
-
-	web.Serve(context)
+	web.Serve()
 }
--- backend/web/serve.go
+++ backend/web/serve.go
@@ -2,23 +2,21 @@ package web
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/go-pg/pg/v10"
+	//"github.com/go-pg/pg/v10"
 	"net/http"
 )
 
-var context *pg.DB
-
-func Serve(dbContext *pg.DB) {
-	context = dbContext
+func Serve() {
 
 	r := gin.Default()
 	r.GET("/ping", ping)
-	r.Run(":8080")
+	r.Run(":8080") //TODO: Make this configurable
 }
 
 func ping(c *gin.Context){
-	//You can work with context in here e.g.
-	//  context.Model(&data.Image{
+	//You can work with dbContext in here e.g.
+	//  databaseContext := data.GetContext()
+	//  databaseContext.Model(&data.Image{
 	//  	Id: 0,
 	//  	DateUploaded: time.Now(),
 	//  	Hash: "SomeHash1234",
