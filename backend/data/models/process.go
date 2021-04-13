77c394276603fbb2cbce2b18587849c287af083f
Lukas Ribi <lukasribi@yahoo.de>
2021-04-13 15:56:34 +0200

    Update process.go

--- backend/data/models/process.go
+++ backend/data/models/process.go
@@ -6,5 +6,6 @@ import (
 
 type Process struct {
 	ProcessId       uuid.UUID        `pg:"type:uuid,default:gen_random_uuid(),pk"`
+	Name            string           `pg:",notnull"`
 	ProcessingSteps []ProcessingStep `pg:"rel:has-many,join_fk:process_id"`
 }
