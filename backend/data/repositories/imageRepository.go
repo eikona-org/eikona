b82cdc175fccec5be55b91923d4fcf8df94c5285
Dominik Castelberg <dominik.castelberg@ost.ch>
2021-04-13 15:38:37 +0200

    Merge ImageRepository

--- backend/data/repositories/imageRepository.go
+++ backend/data/repositories/imageRepository.go
@@ -4,50 +4,91 @@ import (
 	"github.com/google/uuid"
 	"github.com/imgProcessing/backend/v2/data"
 	datamodels "github.com/imgProcessing/backend/v2/data/models"
-	webmodels "github.com/imgProcessing/backend/v2/web/models"
+	"time"
 )
 
 type ImageRepository struct{}
 
-func (r ImageRepository) Find(id uuid.UUID, orgId uuid.UUID) *datamodels.Image {
-	return findImage(id, orgId)
+func (r ImageRepository) Find(id uuid.UUID) *datamodels.Image {
+	return findImage(id)
 }
 
-func (r ImageRepository) AllImages(orgId uuid.UUID) []webmodels.Image {
-	return getAllImages(orgId)
+
+func (r ImageRepository) FindByUserId(id uuid.UUID) *[]datamodels.Image {
+	return findImagesByUserId(id)
 }
 
-func getAllImages(orgId uuid.UUID) []webmodels.Image {
+func (r ImageRepository) FindByOrganizationId(id uuid.UUID) *[]datamodels.Image {
+	return findImagesByOrganizationId(id)
+}
+
+func (r ImageRepository) Insert(name string, minioName string, userId uuid.UUID) *datamodels.Image {
 	dbConnection := data.GetDbConnection()
 	defer dbConnection.Close()
 
-	var imageTest []webmodels.Image
-	err := dbConnection.Model(&datamodels.Image{}).
-		Column("image_id").
-		Column("name").
-		Column("uploaded").
-		Where("owner_id = ?", orgId).
-		Select(&imageTest)
-	if err != nil {
-		return nil
+	transaction, transactionError := dbConnection.Begin()
+	if transactionError != nil {
+		panic(transactionError)
 	}
-	return imageTest
-}
 
-func findImage(id uuid.UUID, orgId uuid.UUID) *datamodels.Image {
-	dbConnection := data.GetDbConnection()
-	defer dbConnection.Close()
-
-	image := &datamodels.Image{ImageId: id}
-	err := dbConnection.Model(image).
-		Relation("Owner").
-		WherePK().
-		Where("owner_id = ?", orgId).
-		Select()
-
-	if err != nil {
-		return nil
+	_, creationError := dbConnection.Model(&datamodels.Image{
+		Name:            name,
+		Uploaded:        time.Now(),
+		MinioObjectName: minioName,
+		OwnerId:         userId,
+	}).Insert()
+	if creationError != nil {
+		transaction.Rollback()
+		panic(creationError)
 	}
+
+	image := &datamodels.Image{}
+	findError := dbConnection.Model(image).Where("minio_object_name = ?", minioName).Select()
+	if findError != nil {
+		transaction.Rollback()
+		panic(findError)
+	}
+
+	transaction.Commit()
 
 	return image
+}
+
+func findImage(id uuid.UUID) *datamodels.Image {
+	dbConnection := data.GetDbConnection()
+	defer dbConnection.Close()
+
+	image := datamodels.Image{ImageId: id}
+	err := dbConnection.Model(image).Select()
+	if err != nil {
+		return nil
+	}
+
+	return &image
+}
+
+func findImagesByOrganizationId(id uuid.UUID) *[]datamodels.Image {
+	dbConnection := data.GetDbConnection()
+	defer dbConnection.Close()
+
+	var images []datamodels.Image
+	err := dbConnection.Model(images).Where("owner_id = ?", id).Select()
+	if err != nil {
+		return nil
+	}
+
+	return &images
+}
+
+func findImagesByUserId(id uuid.UUID) *[]datamodels.Image {
+	dbConnection := data.GetDbConnection()
+	defer dbConnection.Close()
+
+	var images []datamodels.Image
+	err := dbConnection.Model(images).Join("inner join users u on u.user_id = ?", id).Where("owner_id = u.organization_id").Select()
+	if err != nil {
+		return nil
+	}
+
+	return &images
 }
