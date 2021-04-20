297e3de1e087ed660096a965425ae5593cebf382
Pascal Christen <pascal@pascalchristen.ch>
2021-04-20 23:25:31 +0200

    Finalize UI without functionality

--- frontend/src/components/CustomChip.js
+++ frontend/src/components/CustomChip.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 import Chip from '@material-ui/core/Chip'
 import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField'
@@ -6,153 +6,168 @@ import Dialog from '@material-ui/core/Dialog'
 import DialogActions from '@material-ui/core/DialogActions'
 import DialogContent from '@material-ui/core/DialogContent'
 import DialogTitle from '@material-ui/core/DialogTitle'
-import { makeStyles } from '@material-ui/core/styles'
+import useToken from './useToken'
 
-class CustomChip extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            selected: [],
-            data: [
-                {
-                    id: 10,
-                    name: 'Resize',
-                    options: ['width', 'height'],
-                    optionsFilled: ['', ''],
-                    sequence: '',
-                    isSelected: false,
-                    isOpen: false,
-                    color: 'default',
+export default function CustomChip2() {
+    const { token } = useToken()
+    const [data, setData] = useState([])
+    const [selected, setSelected] = useState([])
+    const [errorSteps, setErrorSteps] = useState(null)
+    const [isLoadingSteps, setIsLoadingSteps] = useState(null)
+
+    useEffect(() => {
+        fetch(`https://${window._env_.API_URL}/api/auth/processingsteptypes`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: 'Bearer ' + token,
+            },
+        })
+            .then((res) => res.json())
+            .then(
+                (resultSteps) => {
+                    Object.keys(resultSteps).map((e, i) => {
+                        const length = resultSteps[i]['Options'].length
+                        resultSteps[i].sequence = ''
+                        resultSteps[i].color = 'default'
+                        resultSteps[i].selected = false
+                        resultSteps[i].isOpen = false
+                        resultSteps[i].optionsFilled = Array(length).fill('')
+                    })
+                    setData(resultSteps)
+                    setIsLoadingSteps(false)
                 },
-                { id: 20, name: 'Grayscale', options: [], isSelected: false, isOpen: false, color: 'default' },
-                { id: 30, name: 'Blub', options: [], isSelected: false, isOpen: false, color: 'default' },
-            ],
+                (errorSteps) => {
+                    setIsLoadingSteps(false)
+                    setErrorSteps(errorSteps)
+                }
+            )
+    }, [token])
+
+    const handleChange = (e, dataindex, index) => {
+        updateItemArray(dataindex, 'optionsFilled', index, e.target.value)
+    }
+
+    const handleChangeSequence = (e, dataindex) => {
+        updateItem(dataindex, 'sequence', e.target.value)
+    }
+
+    const handleDialogCancel = (e, dataindex) => {
+        updateItem(dataindex, 'isOpen', false)
+    }
+
+    const updateItemArray = (id, whichvalue, optionindex, newvalue) => {
+        //https://stackoverflow.com/questions/37662708/react-updating-state-when-state-is-an-array-of-objects
+        let index = id
+        if (index !== -1) {
+            let temporaryarray = data.slice()
+            temporaryarray[index][whichvalue][optionindex] = newvalue
+            setData(temporaryarray)
+        } else {
+            console.log('no match')
         }
-        this.handleChipDelete = this.handleChipDelete.bind(this)
-        this.handleChipClick = this.handleChipClick.bind(this)
     }
 
-    callBackMethod() {
-        this.props.sendData(this.state.selected)
+    const updateItem = (id, whichvalue, newvalue) => {
+        //https://stackoverflow.com/questions/37662708/react-updating-state-when-state-is-an-array-of-objects
+        let index = id
+        if (index !== -1) {
+            let temporaryarray = data.slice()
+            temporaryarray[index][whichvalue] = newvalue
+            setData(temporaryarray)
+        } else {
+            console.log('no match')
+        }
     }
 
-    handleChange = (e, dataindex, index) => {
-        const { data } = this.state
-        data[dataindex].optionsFilled[index] = e.target.value
-        this.setState({ data })
-    }
-
-    handleChangeSequence = (e, dataindex) => {
-        const { data } = this.state
-        data[dataindex].sequence = e.target.value
-        this.setState({ data })
-    }
-
-    handleDialogSubmit = (e) => {
+    const handleSubmit = (e, id, index) => {
         e.preventDefault()
-        const { id } = e.currentTarget
-        let tmp = this.state.data
-        tmp[id].isOpen = !tmp[id].isOpen
-        this.setState({ arr: tmp })
-        this.state.open = true
+        var d = data.find((d) => d.Id === id)
+        const selectedHistory = [...selected, d]
+        setSelected(selectedHistory)
+        updateItem(index, 'color', 'primary')
+        updateItem(index, 'selected', true)
+        updateItem(index, 'isOpen', false)
     }
 
-    handleDialogCancel = (e) => {
-        e.preventDefault()
-        const { id } = e.currentTarget
-        let tmp = this.state.data
-        tmp[id].isSelected = !tmp[id].isSelected
-        tmp[id].isOpen = !tmp[id].isOpen
-        tmp[id].color = 'default'
-        this.setState({ arr: tmp })
-    }
-
-    handleSubmit = (e, id, index) => {
-        const { data, selected } = this.state
-        var d = data.find((d) => d.id === id)
-        selected.push(d)
-        this.setState({ data, selected })
-        console.log(this.state.selected)
-        e.preventDefault()
-
-        data[index].isOpen = !data[index].isOpen
-        this.setState({ arr: data })
-        this.state.open = true
-    }
-
-    handleChipClick = (e, options) => {
-        e.preventDefault()
-        const { id } = e.currentTarget
-        let tmp = this.state.data
-        tmp[id].isSelected = !tmp[id].isSelected
-        tmp[id].isOpen = !tmp[id].isOpen
-        tmp[id].color = 'primary'
-        this.setState({ arr: tmp })
-    }
-
-    handleChipDelete = (index) => (event) => {
+    const handleChipClick = (e, index) => {
         console.log(index)
+        updateItem(index, 'isOpen', true)
     }
 
-    render() {
-        return (
-            <>
-                {this.state.data.map((step, index) => (
-                    <>
-                        <Chip
-                            label={step.name}
-                            id={index}
-                            key={index}
-                            clickable
-                            onClick={(e) => this.handleChipClick(e, step.options)}
-                            color={step.color}
-                        />
-                        <Dialog open={step.isOpen} onClose={this.handleClose} aria-labelledby="form-dialog-title">
-                            <DialogTitle id="form-dialog-title">{step.name}</DialogTitle>
-                            <DialogContent>
-                                <TextField
-                                    name="Sequence"
-                                    onChange={(e) => this.handleChangeSequence(e, index)}
-                                    autoFocus
-                                    margin="dense"
-                                    label="Sequence"
-                                    type="number"
-                                    value={step.sequence}
-                                    fullWidth
-                                    required
-                                    key={step.id}
-                                />
-                                {step.options.map((option, i) => (
-                                    <TextField
-                                        margin="dense"
-                                        name={option}
-                                        onChange={(e) => this.handleChange(e, index, i)}
-                                        key={option}
-                                        label={option}
-                                        value={step.optionsFilled[i]}
-                                        type="text"
-                                        fullWidth
-                                    />
-                                ))}
-                            </DialogContent>
-                            <DialogActions>
-                                <Button id={index} onClick={(e) => this.handleDialogCancel(e)}>
-                                    Cancel
-                                </Button>
-                                <Button
+    const handleChipDelete = (index) => {
+        updateItem(index, 'color', 'default')
+        updateItem(index, 'selected', false)
+        console.log('TODO: Remove from array')
+    }
+
+    return (
+        <>
+            {!errorSteps && (
+                <>
+                    {data.map((step, index) => (
+                        <>
+                            {step.selected ? (
+                                <Chip
+                                    label={step.Name}
                                     id={index}
-                                    onClick={(e) => this.handleSubmit(e, step.id, index)}
-                                    color="primary"
-                                >
-                                    Submit
-                                </Button>
-                            </DialogActions>
-                        </Dialog>
-                    </>
-                ))}
-            </>
-        )
-    }
+                                    key={index + 'test'}
+                                    clickable
+                                    onClick={(e) => handleChipClick(e, index)}
+                                    color={step.color}
+                                    onDelete={(e) => handleChipDelete(index)}
+                                />
+                            ) : (
+                                <Chip
+                                    label={step.Name}
+                                    id={index}
+                                    key={index + 'test'}
+                                    clickable
+                                    onClick={(e) => handleChipClick(e, index)}
+                                    color={step.color}
+                                />
+                            )}
+                            <Dialog open={step.isOpen} onClose={handleSubmit} aria-labelledby="form-dialog-title">
+                                <DialogTitle id="form-dialog-title">{step.Name}</DialogTitle>
+                                <DialogContent>
+                                    <TextField
+                                        name="Sequence"
+                                        onChange={(e) => handleChangeSequence(e, index)}
+                                        autoFocus
+                                        margin="dense"
+                                        label="Sequence"
+                                        type="number"
+                                        value={step.sequence}
+                                        fullWidth
+                                        required
+                                        key={step.Id}
+                                    />
+                                    {step.Options.map((option, i) => (
+                                        <TextField
+                                            margin="dense"
+                                            name={option}
+                                            onChange={(e) => handleChange(e, index, i)}
+                                            key={option}
+                                            label={option}
+                                            value={step.optionsFilled[i]}
+                                            type="text"
+                                            fullWidth
+                                        />
+                                    ))}
+                                </DialogContent>
+                                <DialogActions>
+                                    <Button id={index} onClick={(e) => handleDialogCancel(e, index)}>
+                                        Cancel
+                                    </Button>
+                                    <Button id={index} onClick={(e) => handleSubmit(e, step.Id, index)} color="primary">
+                                        OK
+                                    </Button>
+                                </DialogActions>
+                            </Dialog>
+                        </>
+                    ))}
+                </>
+            )}
+        </>
+    )
 }
-
-export default CustomChip
