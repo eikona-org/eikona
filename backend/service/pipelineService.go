06e711f444b4dd9f0d761a03807c3644a2d4daa1
Lukas Ribi <lukasribi@yahoo.de>
2021-04-20 10:48:08 +0200

    Add resize operation to pipelineService

--- backend/service/pipelineService.go
+++ backend/service/pipelineService.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"encoding/json"
 	"github.com/disintegration/gift"
 	data "github.com/imgProcessing/backend/v2/data/models"
 	"github.com/imgProcessing/backend/v2/helper"
@@ -26,9 +27,54 @@ func NewPipelineService(imgWrapper *helper.ImageWrapper, procSteps []data.Proces
 }
 
 func (service *pipelineService) Process() *helper.ImageWrapper {
+	service.applyProcessingSteps()
+
 	service.imageWrapper.ProcessedImage = image.NewRGBA(service.pipeline.Bounds(service.imageWrapper.Image.Bounds()))
 
 	service.pipeline.Draw(service.imageWrapper.ProcessedImage, service.imageWrapper.Image)
 
 	return service.imageWrapper
+}
+
+func (service *pipelineService) applyProcessingSteps() {
+	for _, step := range service.processingSteps {
+		if !isSupportedProcessingStepType(step.ProcessingStepType) {
+			continue
+		}
+
+		// TODO: Refactor
+		if step.ProcessingStepType == data.Resize {
+			service.applyResizeOperation(step.ParameterJson)
+		}
+	}
+}
+
+// TODO: Refactor
+func isSupportedProcessingStepType(procStepType data.ProcessingStepType) bool {
+	return procStepType == data.Resize
+}
+
+// TODO: Refactor
+type resizeParameters struct {
+	Width  int
+	Height int
+}
+
+// TODO: Refactor
+func (service *pipelineService) applyResizeOperation(params string) {
+	b := []byte(params)
+	var parameters resizeParameters
+	err := json.Unmarshal(b, &parameters)
+
+	if err != nil {
+		return
+	}
+
+	service.pipeline.Add(
+		gift.Resize(
+			parameters.Height,
+			parameters.Width,
+			gift.LanczosResampling,
+		),
+	)
 }
