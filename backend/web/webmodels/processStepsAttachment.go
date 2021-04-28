b2f2818dd0d90948dc9a362cafe80d55aef2ee80
Dominik Castelberg <dominik.castelberg@ost.ch>
2021-04-28 11:55:32 +0200

    FIX: swaggo doc

--- backend/web/webmodels/processStepsAttachment.go
+++ backend/web/webmodels/processStepsAttachment.go
@@ -1,5 +1,19 @@
 package webmodels
 
 type ProcessStepsAttachment struct {
-	ProcessingSteps []ProcessStepAttachment `form:"processingSteps" json:"processingSteps" binding:"required" example:"[{ "processId": "ecc20406-a798-11eb-bcbc-0242ac130002", "processingStepType": 10, "executionPosition": 1, "parameterJson": "{ "param": "val" }" }]"`
+	ProcessingSteps []ProcessStepAttachment `form:"processingSteps" json:"processingSteps" binding:"required" example:"{
+	"processingSteps": [
+		{
+			"processId" : "b2058e34-d8fa-44cd-af75-1983a0c5f172",
+			"processingStepType" : 40,
+			"executionPosition" : 2,
+			"parameterJson" : "{}"
+		},{
+			"processId" : "b2058e34-d8fa-44cd-af75-1983a0c5f172",
+			"processingStepType" : 60,
+			"executionPosition" : 3,
+			"parameterJson" : "{ 'var': 'val' }"
+		}
+	]
+}"`
 }
